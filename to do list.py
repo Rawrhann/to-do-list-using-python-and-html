c269b774c0b2039025ea20ee5853ca5b11494a18
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-30 01:13:07 +0800

    pseudo code

--- to do list.py
+++ to do list.py
@@ -1,10 +1,20 @@
-"""Final Project
+"""
+Final Project
 
 Gawa kayo ng medyo exciting na python program. Hanap kayo ng any existing python project sa youtube na interesting para sa inyo. Gawin nyo yung project tapos pag gumagana na gawan nyo ng demo.
 
 Pag send nyo ng demo sakin, kindly add some details on chat kung saan nyo pweding iapply ung natutunan nyo sa project na sinundan nyo.
 Please walang magda-download ng source code. Kaylangan nyong itype at mag git commit pa din every small milestone.
 Push nyo yung code sa github. Lagay nyo sa README yung youtube link or icomment nyo sa code to give credit.
-Send the demo/link to my messenger before Feb 3."""
+Send the demo/link to my messenger before Feb 3.
+"""
+
+#create a to do list located in a website using python and html
+
+#create a function that could input a to do
+#create a function that could delete inputed to do
+#create a function that could edit a to do
+
+#create a checker that is interactable with the user to checkoff the to do
 
 
