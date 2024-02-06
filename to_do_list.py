66f2aa6dc86d2062fb2e8011fb79e6197b00d60e
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-02-07 01:29:04 +0800

    code not working when adding new to do lists

--- to_do_list.py
+++ to_do_list.py
@@ -27,13 +27,13 @@ todos = [{"task": "Sample Todo", "done": False}]
 def index():
     return render_template("index.html", todos=todos)
 
-@app.route("/add", methods = ["Post"])
+@app.route("/add", methods=["POST"])
 def add():
     todo = Request.form['todo']
-    todos.append({"task": todo, "done":False})
+    todos.append({'task': todo, "done": False})
     return redirect(url_for("index"))
 
-@app.route("/edit/<int:index", methods=["GET", "POST"])
+@app.route("/edit/<int:index>", methods=["GET", "POST"])
 def edit(index):
     todo = todos[index]
     if Request.method == "POST":
@@ -42,6 +42,15 @@ def edit(index):
     else:
         return render_template("edit.html", todo=todo, index=index)
 
+@app.route("/check/<int:index>")
+def check(index):
+    todos[index]['done'] = not todos[index]['done']
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:index>")
+def delete(index):
+    del todos[index]
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True)
